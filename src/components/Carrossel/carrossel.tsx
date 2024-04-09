5c49ea24894d474d87d8bc0004b00d25889247eb
iza <115378274+izabartolini@users.noreply.github.com>
2024-04-09 16:26:44 -0300

    Ajustes no carrossel

--- src/components/Carrossel/carrossel.tsx
+++ src/components/Carrossel/carrossel.tsx
@@ -2,9 +2,9 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import styles from './carrossel.module.css'
-import feedback1 from '@/assets/feedback1.png'
-import feedback2 from '@/assets/feedback2.jpg'
-import feedback3 from '@/assets/feedback3.jpg'
+import cachorro from '@/assets/cachorro.png'
+import fumaca from '@/assets/fumaça.png'
+import penguim from '@/assets/penguim.png'
 import { useState } from 'react';
 import { Carousel, Image } from 'react-bootstrap';
 
@@ -18,13 +18,13 @@ function Carrossel() {
     return (
         <Carousel activeIndex={index} onSelect={handleSelect} className={styles.carrosselContainer} >
             <Carousel.Item className={styles.carrosselItem}>
-                <Image src={feedback1.src} fluid className={styles.carroselImg}/>
+                <Image src={cachorro.src} fluid className={styles.carroselImg}/>
             </Carousel.Item>
             <Carousel.Item className={styles.carrosselItem}>
-            <Image src={feedback2.src} fluid className={styles.carroselImg}/>
+            <Image src={fumaca.src} fluid className={styles.carroselImg}/>
             </Carousel.Item>
             <Carousel.Item className={styles.carrosselItem}>
-            <Image src={feedback3.src} fluid className={styles.carroselImg}/>
+            <Image src={penguim.src} fluid className={styles.carroselImg}/>
             </Carousel.Item>
         </Carousel>
     );

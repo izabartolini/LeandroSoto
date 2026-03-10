db654648c129aa680c4b91b9fca1206b4ce7490f
izabartolini <bartoliniizabelly@gmail.com>
2026-03-10 17:24:37 -0300

    Mudanças de importação, alteração de icone do site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import styles from "./page.module.css";
-import logo from "@/assets/logo-pnf.png"
-import consumidor from "@/assets/direito-consumidor.jpg"
-import civil from "@/assets/direitos-civis.jpg"
-import familiar from "@/assets/direito-familiar.jpg"
-import trabalhistas from "@/assets/direitos-trabalhistas.jpg"
-import leandro from "@/assets/advogado-png.png"
+import  "@/assets/logo-pnf.png"
+import  "@/assets/direito-consumidor.jpg"
+import  "@/assets/direitos-civis.jpg"
+import "@/assets/direito-familiar.jpg"
+import  "@/assets/direitos-trabalhistas.jpg"
+import  "@/assets/advogado-png.png"
 import Carrossel from "@/components/Carrossel/carrossel";
 import Mapa from "@/components/Mapa/mapa";
 import Footer from "@/components/footer/footer";
@@ -19,7 +19,7 @@ export default function Home() {
 
 
       <div className={styles.faleconosco}>
-        <img src={logo.src} alt="" />
+        <img src={"@/assets/logo-pnf.png"} alt="" />
         <p>Nos dedicamos a defender os direitos dos clientes com expertise, proporcionando representação legal excepcional e resultados positivos. Comprometemo-nos a compreender suas necessidades individuais e a desenvolver estratégias jurídicas personalizadas para garantir justiça e equidade em todos os casos.</p>
         <a href="https://web.whatsapp.com/"><button>Fale conosco</button></a>
       </div>
@@ -30,25 +30,25 @@ export default function Home() {
         </h1>
         <div className={styles.direitosContainer}>
           <div className={styles.direito}>
-            <img src={consumidor.src} alt="" />
+            <img src={"@/assets/direito-consumidor.jpg"} alt="" />
             <h2>Direito do consumidor</h2>
             <p>Buscamos garantir justiça e compensação justa para os consumidores em transações comerciais, defendendo seus direitos.
 
             </p>
           </div>
           <div className={styles.direito}>
-            <img src={civil.src} alt="" />
+            <img src={"@/assets/direitos-civis.jpg"} alt="" />
             <h2>Direitos civis</h2>
             <p>Nosso compromisso abrange a defesa dos direitos civis, visando garantir igualdade, liberdade e justiça para todos os cidadãos.</p>
           </div>
           <div className={styles.direito}>
-            <img src={familiar.src} alt="" />
+            <img src={"@/assets/direito-familiar.jpg"} alt="" />
             <h2>Direito familiar</h2>
             <p>Buscamos proteger os direitos familiares, garantindo soluções justas em questões como divórcio, guarda de crianças e pensão alimentícia.
             </p>
           </div>
           <div className={styles.direito}>
-            <img src={trabalhistas.src} alt="" />
+            <img src={"@/assets/direitos-trabalhistas.jpg"} alt="" />
             <h2>Direitos trabalhistas</h2>
             <p>Em nossos objetivos inclui a defesa dos direitos trabalhistas, assegurando condições de trabalho justas e equitativas.</p>
           </div>
@@ -61,7 +61,7 @@ export default function Home() {
           <p>Como advogado, minha formação consiste em anos de estudo em Direito, obtendo uma base sólida e diversificada. Graduei-me em uma instituição renomada, onde participei ativamente de atividades extracurriculares, aprimorando minhas habilidades práticas. Ingressei em um escritório de advocacia prestigiado após a graduação, onde tive a oportunidade de representar uma ampla gama de clientes, desde indivíduos até grandes empresas. Ao longo dos anos, adquiri experiência em diversas áreas do direito, incluindo litígios civis, questões trabalhistas e empresariais. Minha trajetória profissional é marcada por desafios superados e casos bem-sucedidos, nos quais defendi os interesses dos meus clientes com dedicação e ética. Estou sempre me atualizando e aprimorando, participando de cursos e conferências para oferecer o melhor serviço possível. Como advogado, minha missão é promover a justiça, proteger os direitos dos meus clientes e contribuir para uma sociedade mais equitativa. Cada caso representa uma nova oportunidade de fazer a diferença e deixar um impacto positivo no mundo através do meu trabalho jurídico.</p>
         </div>
         <div className={styles.imagemLeandro}>
-          <img src={leandro.src} alt="" />
+          <img src={"@/assets/advogado-png.png"} alt="" />
         </div>
       </div>
 
